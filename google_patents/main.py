2a1c10566a33d2da4b5b5cbe5de8368bd21ee485
Sprise233 <77605532+Sprise233@users.noreply.github.com>
2023-12-01 16:56:32 +0800

    Delete google_patents directory

--- google_patents/main.py
+++
@@ -1,7 +0,0 @@
-from google_patents.google_carwlers.google_crawlers import GoogleCrawlers
-from google_patents.utils.utils import init
-
-if __name__ == '__main__':
-    mysql_pool, config, pub_ids = init("config.ini")
-    GoogleCrawlers(mysql_pool, config, pub_ids).start()
-    mysql_pool.close_all_connections()
